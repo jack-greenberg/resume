0079fa9782a4f14770d2af33e7250c26224a4168
Jack Greenberg <j.lester.greenberg@gmail.com>
2020-06-13 10:24:00 -0700

    Update templating and add pdf generation

--- resume/app.py
+++ resume/app.py
@@ -2,13 +2,33 @@ import click
 from jinja2 import Template, Environment, PackageLoader
 from pathlib import Path
 import yaml
+import pdfkit
 
 DATA_PATH = Path("data/")
+BUILD_PATH = Path("build/")
+
+PDF_OPTIONS = {
+    'margin-top': '0',
+    'margin-right': '0',
+    'margin-bottom': '0',
+    'margin-left': '0',
+}
+
+env = Environment(loader=PackageLoader("resume", "templates"))
 
 
-def get_resume(file):
+def read_file(file):
     file = DATA_PATH / file
     return open(file, "r").read()
+
+
+def save_file(file, text):
+    with open(file, "w+") as f:
+        f.write(text)
+
+
+def convert_to_pdf(file):
+    pdfkit.from_file(str(file), str(BUILD_PATH / "build.pdf"), options=PDF_OPTIONS)
 
 
 def parse_yaml(y):
@@ -17,11 +37,15 @@ def parse_yaml(y):
 
 @click.command()
 def main():
-    env = Environment(loader=PackageLoader("resume", "templates"))
     template = env.get_template("universal-resume.j2")
-    resume_file = get_resume("resume.yaml")
-    rendered = template.render(r=parse_yaml(resume_file))
-    print(rendered)
+
+    resume_file = read_file("resume.yaml")
+    rendered = template.render(
+        r=parse_yaml(resume_file), template_name="universal-resume"
+    )
+
+    save_file(BUILD_PATH / "build.html", rendered)
+    convert_to_pdf(BUILD_PATH / "build.html")
 
 
 if __name__ == "__main__":
